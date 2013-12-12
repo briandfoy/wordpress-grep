cbb01683e1151c7a578ea98c44c48f5ce15b7f4a
brian d foy <brian.d.foy@gmail.com>
2013-12-12 12:24:11 -0500

    Formatting cleanups

--- lib/Wordpress/Grep.pm
+++ lib/Wordpress/Grep.pm
@@ -3,6 +3,7 @@ use v5.16;
 use strict;
 use warnings;
 
+use utf8;
 use subs qw();
 use vars qw($VERSION);
 
@@ -119,27 +120,27 @@ These parameters have defaults
 
 sub connect {
 	my( $class, %args ) = @_;
-	
-	foreach my $required ( qw(user password database) ) {
-		croak "You must set '$required' in connect()" 
+
+	foreach my $required ( qw(user database) ) {
+		croak "You must set '$required' in connect()"
 			unless defined $args{$required};
 		}
 
 	$args{host} //= 'localhost';
 	$args{port} //= 3306;
-	
+
 	my $dsn = "dbi:mysql:db=$args{database};host=$args{host};port=$args{port}";
 
 	#dbi:DriverName:database_name
 	#dbi:DriverName:database_name@hostname:port
 	#dbi:DriverName:database=database_name;host=hostname;port=port
-	
+
 	my $db = DBI->connect( $dsn, $args{user}, $args{password} );
 	croak "Could not connect to database [$args{host}:$args{port}]\n$DBI::Error"
 		unless defined $db;
-	my $self = bless { 
+	my $self = bless {
 		db       => $db,
-		args     => \%args, 
+		args     => \%args,
 		}, $class;
 	$self->_db_init;
 
@@ -148,13 +149,13 @@ sub connect {
 
 sub _db_init {
 	my( $self ) = @_;
-	
+
 	$self->_db_utf8;
 	}
 
 sub _db_utf8 {
 	my( $self ) = @_;
-	
+
 	my $sql = qq{SET NAMES 'utf8';};
 	$self->db->do($sql);
 	$self->db->{'mysql_enable_utf8'} = 1;
@@ -205,18 +206,13 @@ sub search {
 	$self->_check_args;
 
 	my $query = $self->_get_sql;
-	
+
 	# filter results by the LIKE, directly in the SQL
 	$query .= $self->_like_where_clause if defined $args{sql_like};
 	$self->_set_query( $query );
-	
-	#XXX Can I figure out the number dynamically in a better way?
-	my $param_count = () = $query =~ /\?/g;
-	$self->_set_bind_params( [ ( $args{sql_like} ) x $param_count ] ) 
-		if defined $args{sql_like};
 
 	my $posts = $self->_get_posts;
-say "There are " . keys(%$posts) . " posts in the module";
+
 	# filter posts by the regex
 	if( defined $self->_args->{regex} ) {
 		my $re = $self->_args->{regex};
@@ -233,7 +229,7 @@ say "There are " . keys(%$posts) . " posts in the module";
 	# filter posts by the sub
 	if( defined $args{code} ) {
 		foreach my $post_id ( keys %$posts ) {
-			delete $posts->{$post_id} 
+			delete $posts->{$post_id}
 				unless $args{code}->( $posts->{$post_id} );
 			}
 		}
@@ -245,7 +241,14 @@ say "There are " . keys(%$posts) . " posts in the module";
 
 sub _query { exists $_[0]->{query} ? $_[0]->{query} : '' }
 sub _set_query {
-	$_[0]->{query} = $_[1];
+	my( $self, $query ) = @_;
+
+	#XXX Can I figure out the number dynamically in a better way?
+	my $param_count = () = $query =~ /\?/g;
+	$self->_set_bind_params( [ ( $self->_args->{sql_like} ) x $param_count ] )
+		if defined $self->_args->{sql_like};
+
+	$self->{query} = $query;
 	}
 
 sub _bind_params { exists $_[0]->{bind_params} ? @{$_[0]->{bind_params}} : () }
@@ -267,34 +270,34 @@ sub _clear_search {
 	my @clear_keys = qw( args sql bind_params );
 	delete @{ $_[0] }{ @clear_keys };
 	}
-	
+
 sub _check_args {
 	my( $self ) = @_;
-	
+
 	if( exists $self->_args->{regex} ) {
-		croak "'regex' value must be a regex reference" 
-			unless ref $self->_args->{regex} eq ref qr//;	
+		croak "'regex' value must be a regex reference [@{[$self->_args->{regex}]}]"
+			unless ref $self->_args->{regex} eq ref qr//;
 		}
 
 	if( exists $self->_args->{code} ) {
-		croak "'code' value must be a code reference" 
+		croak "'code' value must be a code reference"
 			unless ref $self->_args->{code} eq ref sub {};
 		}
 
 	my @array_keys = qw( type include_columns exclude_columns );
 	foreach my $array_arg ( @array_keys ) {
 		next unless exists $self->_args->{$array_arg};
-		croak "'array_arg' value must be an array reference" 
+		croak "'array_arg' value must be an array reference"
 			unless ref $self->_args->{$array_arg} eq ref [];
 		}
-		
+
 	return 1;
 	}
 
-sub _get_sql {	
+sub _get_sql {
 	'SELECT * FROM wp_posts WHERE post_type = "post"'
 	}
-	
+
 sub _like_where_clause {
 	' AND (post_title LIKE ? OR post_content LIKE ?)'
 	}
@@ -302,14 +305,14 @@ sub _like_where_clause {
 sub _get_posts {
 	my( $self ) = @_;
 	my $sth = $self->db->prepare( $self->_query );
-	croak 
+	croak
 		"Could not create statement handle\n\n" .
 		"DBI Error: $DBI::Error\n\n" .
 		"Statement-----\n@{[$self->_query]}\n-----\n"
 		unless defined $sth;
 	$sth->execute( $self->_bind_params );
-	
-	my $posts = $sth->fetchall_hashref( 'ID' );	
+
+	my $posts = $sth->fetchall_hashref( 'ID' );
 	}
 
 =back
